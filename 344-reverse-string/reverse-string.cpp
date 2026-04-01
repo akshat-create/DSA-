f1de42544528b6d0fbe1a3b5cb3816ea47e9f907
Akshat Pandey <akshatpandey9198@gmail.com>
2026-04-01 10:55:05 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (15.50%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.size();
         int l = 0;
         int r = n-1;
-        while(l<r){
+        while(l<=r){
             swap(s[l],s[r]);
             l++;
             r--;
