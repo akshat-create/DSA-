fcdef7c5f472007deb8c847d70e6d4cc5c5c74bd
Akshat Pandey <akshatpandey9198@gmail.com>
2026-07-01 21:15:58 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (89.31%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev1 = 0, prev2 = 0;
+
+        for(int money : nums) {
+            int curr = max(prev1, money + prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
