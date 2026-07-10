7f13eee1740b6b5bd11f04c6e376e4ae2570f305
Akshat Pandey <akshatpandey9198@gmail.com>
2026-07-10 21:51:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (92.86%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -4,12 +4,12 @@ public:
 
         int n = triangle.size();
 
-        for(int i = n - 2; i >= 0; i--) {
+        for (int i = n - 2; i >= 0; i--) {
 
-            for(int j = 0; j <= i; j++) {
+            for (int j = 0; j <= i; j++) {
 
-                triangle[i][j] += min(triangle[i+1][j],
-                                       triangle[i+1][j+1]);
+                triangle[i][j] +=
+                    min(triangle[i + 1][j], triangle[i + 1][j + 1]);
             }
         }
 
