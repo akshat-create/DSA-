2cf3c7dd51a2c9519abf4be936ffe8aaaf7a95b4
Akshat Pandey <akshatpandey9198@gmail.com>
2026-07-26 16:03:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (40.00%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,29 +1,61 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+void reverse(ListNode*head,int times){
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+        while(times--){
+            ListNode*nex=curr->next;
+            curr->next=prev;
+            prev=curr;curr=nex;
+        }    return ;
+        }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (!head) return nullptr;
+          int size=k;
+        if(head == NULL)
+        return head;
+        ListNode* left=head;
+        ListNode* res=NULL;
+        ListNode* prevleft = NULL;
+        ListNode*right;
 
-        ListNode* tail = head;
-        for (int i = 0; i < k; ++i) {
-            if (!tail) return head;
-            tail = tail->next;
+        while(true){
+            right = left;
+            for(int i = 0; i<size-1;i++){
+                if(right==NULL)
+                break;
+                right=right->next;
+
+            }
+            if(right){
+                ListNode*nextleft=right->next;
+                reverse(left,size);
+                if(prevleft)
+                prevleft->next=right;
+                prevleft=left;
+                if(res==NULL)
+                res=right;
+                left=nextleft;}
+                else{
+                    if(prevleft)
+                    prevleft->next=left;
+                    if(res==NULL)
+                    res=left;
+                    break;
+                }
+            }
+            return res;
         }
 
-        ListNode* newHead = reverse(head, tail);
-        head->next = reverseKGroup(tail, k);
-        return newHead;
-    }
 
-private:
-    ListNode* reverse(ListNode* cur, ListNode* end) {
-        ListNode* prev = nullptr;
-        while (cur != end) {
-            ListNode* next = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = next;
-        }
-        return prev;
-    }
+    
 };
